5d5045454ec48eea83dfa4b083b56cc619c934be
christy castro <christy@aureolab.cl>
2023-04-18 00:14:41 -0500

    js

--- src/js/script.js
+++ src/js/script.js
@@ -6,41 +6,56 @@ $('.close').click(function(){
     $('.menu').removeClass('enter')
 })
 
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-	    var target = this.hash;
-	    var $target = $(target);
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top - 90
-	    }, 700, 'swing', function(){
-			window.location.hash = './' + target;
-            $('.menu').removeClass('enter')
-			console.log('11111111')
-        });
+$('.menu a').on('click',function (e) {
+	e.preventDefault();
+	var target 	= this.hash;
+	var $target = $(target);
+	$('html, body').stop().animate({
+		'scrollTop': $target.offset().top - 90
+	}, 500, 'swing', function(){
+		$('.menu').removeClass('enter')
 	});
-	$('.menu a').on('click', function(){
-		$('.menu a').removeClass('active')
-		$(this).addClass('active')
-	});
+	// console.log(this.hash)
+	// if (window.location.pathname == "/") {
+	// 	e.preventDefault();
+		
+	// 	var hash = this.hash
+		
+	// 	$('html, body').stop().animate({
+	// 		scrollTop: $(hash).offset().top - 90
+	// 	}, 500, function(){
+			
+	// 	});
+	// 	console.log('aaaaaaaaaaaa')
+	// } else{
+	// 	// window.location.href = './';
+	// 	var uno = window.location.href = './'
+	// 	// var dos = uno = './'
+	// 	// dos
+	// }
+});
 
-	$('.open-popup').magnificPopup({
-		type: 'image',
-		mainClass: 'mfp-with-zoom', 
-		gallery:{
-					enabled:true
-				},
-		
-		zoom: {
-			enabled: true,
-			duration: 300, // duration of the effect, in milliseconds
-			easing: 'ease-in-out', // CSS transition easing function
-		
-			opener: function(openerElement) {
-		
-				return openerElement.is('img') ? openerElement : openerElement.find('img');
-			}
+$('.menu a').on('click', function(){
+	$('.menu a').removeClass('active')
+	$(this).addClass('active')
+});
+
+$('.open-popup').magnificPopup({
+	type: 'image',
+	mainClass: 'mfp-with-zoom', 
+	gallery:{
+				enabled:true
+			},
+	
+	zoom: {
+		enabled: true,
+		duration: 300, // duration of the effect, in milliseconds
+		easing: 'ease-in-out', // CSS transition easing function
+	
+		opener: function(openerElement) {
+	
+			return openerElement.is('img') ? openerElement : openerElement.find('img');
 		}
-	});
+	}
 });
 
